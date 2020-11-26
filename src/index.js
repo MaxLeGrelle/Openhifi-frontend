54d781d4f0819ae326b164084b531ac901c4f06d
unknown <thomas@dorrekens.com>
2020-11-26 18:01:56 +0100

    navbar : ok and debug menu + main

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./stylesheets/style.css";
 import "bootstrap"
-import logo from "./img/open-hifi-logo-transparent1.png"
+import logo from "./img/open-hifi-logo-transparent2.png"
+import profile from "./img/default_profile.png"
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
@@ -11,16 +12,21 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 
 
 function displayAcceuil() {
+    //set up for import fas
     library.add(fas)
     dom.watch()
     $('#logo').append(`<img src="${logo}" alt="logo" height="100px" width="100px"/>`)
     $("#search").append(`<div class="input-group md-form form-sm form-2 pl-0">
-    <input class="form-control my-0 py-1 red-border" type="text" placeholder="Search" aria-label="Search">
+    <input size ="100" class="form-control my-0 py-1 red-border" type="text" placeholder="Rechercher" aria-label="Search">
     <div class="input-group-append">
       <span class="input-group-text red lighten-3" id="basic-text1"><i class="fas fa-search text-grey"
           aria-hidden="true"></i></span>
     </div>
   </div>`)
+  $('#add').append(`<button class="btn btn-bluegradient">Ajouter</button>`)
+  $('#profile').append(`<img src="${profile}" alt="logo" height="60px" width="60px"/>`)
+
+  
 }
 
 displayAcceuil()
