0604f7af9236bf0131438aa28c94e2a7c491b435
le Grelle Max <max.legrelle@student.vinci.be>
2020-11-26 12:52:07 +0100

    add - init grid pageAcceuil

--- src/index.js
+++ src/index.js
@@ -1,42 +1,12 @@
 import "./stylesheets/style.css";
 import "bootstrap"
-import logo from "./img/js-logo.png";
-import musicPath from "./sound/Infecticide-11-Pizza-Spinoza.mp3";
-import {AnalogClock} from "customizable-analog-clock";
+import logo from "./img/open-hifi-logo-transparent.png"
 
-
-function displayIndex() {
-    const lecteur = `<audio id="audioPlayer" controls autoplay>
-    <source
-      src="${musicPath}"
-      type="audio/mpeg"
-    />
-    Your browser does not support the audio element.
-    </audio>`;
-    const jsImage = `<img src="${logo}" height="50" alt="" />`;
-
-    $(".lecteur").append(lecteur);
-    $(".jsImage").append(jsImage);
+function displayAcceuil() {
+    $(".container").append(`<div class='logo'>
+        <img src='${logo}' alt='logo' width='150px' height='150px'>
+        </div>
+    `)
 }
 
-const stopStartSound = () => {
-    const myAudioPlayer = document.querySelector("#audioPlayer");
-    
-    if( myAudioPlayer.paused)
-        myAudioPlayer.play();
-    else
-        myAudioPlayer.pause();
-}
-
-displayIndex();
-
-const header = document.querySelector("header");
-
-header.addEventListener("click",stopStartSound);
-
-$("#myClock").attr("style", "width : 200px; height : 200px");
-
-const clock = new AnalogClock({
-    htmlElement : "myClock",
-    showIndicators : true,
-})
+displayAcceuil()
