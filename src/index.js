cf5668fca0f677e115d3b09583e6f104c9d1c0dc
unknown <thomas@dorrekens.com>
2020-11-26 15:00:16 +0100

    bootstrap working

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import "./stylesheets/style.css";
 import "bootstrap"
 import logo from "./img/open-hifi-logo-transparent1.png"
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function displayAcceuil() {
     $('#logo').append(`<img src="${logo}" alt="logo" height="100px" width="100px"/>`)
-    $("#search").append(``)
+    $("#search").append(`<div class="active-pink-3 active-pink-4 mb-4">
+    <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+  </div>`)
 }
 
 displayAcceuil()
