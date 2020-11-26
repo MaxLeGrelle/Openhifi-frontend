cc7b24ae94014e87e8c85ab3dbe484c87ceb30d0
unknown <thomas@dorrekens.com>
2020-11-26 14:47:44 +0100

    first impression front-end

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import "./stylesheets/style.css";
 import "bootstrap"
-import logo from "./img/open-hifi-logo-transparent.png"
+import logo from "./img/open-hifi-logo-transparent1.png"
+import 'bootstrap';
 
 function displayAcceuil() {
-    
+    $('#logo').append(`<img src="${logo}" alt="logo" height="100px" width="100px"/>`)
+    $("#search").append(``)
 }
 
 displayAcceuil()
