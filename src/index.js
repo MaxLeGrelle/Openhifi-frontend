d63abf71e1505098821b050faa98fa37a3c188ab
le Grelle Max <max.legrelle@student.vinci.be>
2020-11-26 13:15:12 +0100

    reset - acceuil

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import "bootstrap"
 import logo from "./img/open-hifi-logo-transparent.png"
 
 function displayAcceuil() {
-    $(".container").append(`<div class='logo'>
-        <img src='${logo}' alt='logo' width='150px' height='150px'>
-        </div>
-    `)
+    
 }
 
 displayAcceuil()
