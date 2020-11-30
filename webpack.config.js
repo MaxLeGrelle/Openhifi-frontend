28f08df70e29e250d85791ad265827f7e2197fc1
le Grelle Max <max.legrelle@student.vinci.be>
2020-11-30 19:13:07 +0100

    refactor - change port to enable connexion with the backend

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     historyApiFallback: true, // serve index.html instead of routes leading to no specific ressource
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:9090",
         //pathRewrite: {'^/api' : ''}
       },
     },
