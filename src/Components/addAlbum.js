87a8d5cb22405146b0b60d8cbe9a8e9fe9a351dd
le Grelle Max <max.legrelle@student.vinci.be>
2020-12-04 22:30:19 +0100

    add - possibility to add musics when creating album & encoding sounds in base64

--- src/Components/addAlbum.js
+++ src/Components/addAlbum.js
@@ -4,33 +4,78 @@ const jwt = require("jsonwebtoken");
 
 function displayAddAlbum() {
     $("#page").append(`
-    <form id="formAddAlbum">
-        <div class="form-group">
-            <label for="nom">Nom :</label>
-            <input type="text" class="form-control" id="nom">
-        </div>
-        <div class="form-group">
-            <label for="image">Image :</label>
-            <input type="file" class="form-control" id="image">
-        </div>
-        <div class="form-group">
-            <input type="submit" class="form-control" id="submitAddAlbum">
-        </div>
-    </form>
-    <div id="errorAddingAlbum"></div>
-    <div id="displayAlbum"></div>`)
-    $("#formAddAlbum").on("submit", onSubmitAlbum);
+    <div class="container">
+        <form id="formAddMusic">
+            <div class="form-group">
+                <label for="musicTitle">Titre :</label>
+                <input type="text" class="form-control" id="musicTitle">
+            </div>
+            <div class="form-group">
+                <label for="music">Musique :</label>
+                <input type="file" class="form-control" id="music">
+            </div>
+            <div class="form-group w-25">
+                <input value="Ajouter la musique" type="submit" class="form-control" id="submitAddMusic">
+            </div>
+        </form>
+        <ol id="listMusicsToAdd">
+        </ol>
+    </div>`)
+    $("#formAddMusic").on("submit", onSubmitMusic);
+}
+
+let listMusicToAdd;
+
+function onSubmitMusic(e) {
+    e.preventDefault();
+    let showFormAddAlbum;
+    if (!listMusicToAdd) {
+        listMusicToAdd = new Array();
+        showFormAddAlbum = true;
+    }
+    if (showFormAddAlbum) {
+        $("#listMusicsToAdd").append(`
+        <div class="container">
+            <form id="formAddAlbum">
+                <div class="form-group">
+                    <label for="nom">Nom :</label>
+                    <input type="text" class="form-control" id="nom">
+                </div>
+                <div class="form-group">
+                    <label for="image">Image :</label>
+                    <input type="file" class="form-control" id="image">
+                </div>
+                <div class="form-group w-25">
+                    <input value="Créer l'album" type="submit" class="form-control" id="submitAddAlbum">
+                </div>
+            </form>
+            <div id="errorAddingAlbum"></div>
+        </div>`)
+        $("#formAddAlbum").on("submit", onSubmitAlbum);
+        showFormAddAlbum = false
+    }
+    $("#listMusicsToAdd").append(`
+            <li>${$("#musicTitle").val()}</li>
+    `)
+    const promise = fileToBase64($("#music").prop('files')[0]);
+    promise.then((music64) => {
+        listMusicToAdd.push({
+            "music64" : music64,
+            "title" : $("#musicTitle").val()
+        })
+    })
+    
 }
 
 function onSubmitAlbum(e) {
     e.preventDefault();
-    const promise = imageToBase64($("#image").prop('files')[0]);
+    const promise = fileToBase64($("#image").prop('files')[0]);
     promise.then((image64) => {
         const user = getUserStorageData();
         const userPayload = jwt.decode(user.token)
         let album = {
             name : $("#nom").val(),
-            listIdMusics : [],
+            listMusics : listMusicToAdd,
             idCreator : userPayload.id,
             image64 : image64,
             imageName : $("#image").prop('files')[0].name
@@ -61,12 +106,12 @@ function onErrorAddingAlbum(err){
     $("#errorAddingAlbum").append(`<p class="alert alert-danger mt-3"> ${err.message} </p>`);
 }
 
-function imageToBase64(image) {
+function fileToBase64(file) {
     return new Promise(function(resolve, reject) {
         var reader = new FileReader();
         reader.onload = function() { resolve(reader.result); };
         reader.onerror = reject;
-        reader.readAsDataURL(image);
+        reader.readAsDataURL(file);
     });
 }
 export default displayAddAlbum;
