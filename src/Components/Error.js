014ef650ffbc63d9aeedee052e35370a5849e98e
@thomas.lempereur <thomas.lempereur@student.vinci.be>
2020-12-12 15:15:10 +0100

    display error

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,8 +1,5 @@
 
 function displayError(err){
-    $("#navbar").empty();
-    $("#menu").empty();
-    $("#footer").empty();
     if(!err) $("#page").html(`<p class ="alert alert-danger"> oups, il y a eu une erreur <h1> (╬▔皿▔)╯ </h1> </p>`);
     else if (!err.message)  $("#page").html(`<p class ="alert alert-danger" > ${err} <h1> (╬▔皿▔)╯ </h1></p>`)
     else  $("#page").html(`<p class ="alert alert-danger"> ${err.message}  <h1 id="error"> (╬▔皿▔)╯ </h1></p>`);
