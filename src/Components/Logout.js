0f4819680fd3753931d7f0d52ecc3f78aa84d591
le Grelle Max <max.legrelle@student.vinci.be>
2020-12-13 10:36:02 +0100

    refactor - comment logout.js

--- src/Components/Logout.js
+++ src/Components/Logout.js
@@ -1,7 +1,9 @@
 const { removeAllDataStorage} = require("../Utils/storage");
 const { redirectUrl } = require("./Router");
 
-//redirect to login page and logout the currrent user
+/**
+ * redirect to login page and logout the currrent user
+ */
 function logout() {
     removeAllDataStorage();
     redirectUrl("/login")
